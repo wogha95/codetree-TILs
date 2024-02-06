662bf1578d3de17ff85bb9f2df984589356f9ff6
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-02-06 10:05:57 +0900

    Title: 최소공배수 구하기, Time: 193ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240206/최소공배수 구하기/find-the-least-common-multiple.js
@@ -0,0 +1,24 @@
+const lines = require("fs").readFileSync(0).toString().trim().split("\n")
+lines.forEach(line => {
+    const [n, m] = line.split(' ');
+    console.log(LCM(n, m));
+})
+
+function LCM (n, m) {
+    const [max, min] = [Math.max(n, m), Math.min(n, m)];
+    return n * m / GCD(max, min);
+}
+
+function GCD (max, min) {
+    if (min === 1) {
+        return min;
+    }
+
+    while (min !== 0) {
+        const temp = max % min;
+        max = min;
+        min = temp;
+    }
+    
+    return max;
+}
