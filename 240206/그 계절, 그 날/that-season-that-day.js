d18cc4fc0df7bb070706adc7abf38e1f1019a765
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-02-06 20:28:33 +0900

    Title: 그 계절, 그 날, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240206/그 계절, 그 날/that-season-that-day.js
+++ 240206/그 계절, 그 날/that-season-that-day.js
@@ -13,13 +13,22 @@ function checkExistence(year, month, day) {
         return day <= 31;
     }
 
-    if (month === 2) {
-        const isLeapYear1 = year % 4 === 0 && year % 100 !== 0;
-        const isLeapYear2 = year % 4 === 0 && year % 100 === 0 && year % 400 === 0;
-        return isLeapYear2 || isLeapYear1 ? day <= 29 : day <= 28;
+    if (month !== 2) {
+        return day <= 30;
     }
 
-    return day <= 30;
+    const isLeapYear3 = year % 4 === 0 && year % 100 === 0 && year % 400 === 0;
+    if (isLeapYear3) {
+        return day <= 29;
+    }
+    
+    const isLeapYear2 = year % 4 === 0 && year % 100 === 0;
+    if (isLeapYear2) {
+        return day <= 28;
+    }
+
+    const isLeapYear1 = year % 4 === 0;
+    return isLeapYear1 ? day <= 29 : day <= 28;
 }
 
 function getSeason(month) {
