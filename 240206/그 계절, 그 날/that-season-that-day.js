fd42eaf6894d6e270e485fd64b2640046d6b7dc6
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-02-06 20:11:03 +0900

    Title: 그 계절, 그 날, Time: 197ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240206/그 계절, 그 날/that-season-that-day.js
@@ -0,0 +1,33 @@
+const lines = require("fs").readFileSync(0).toString().split("\n")
+lines.forEach(line => {
+    const [year, month, day] = line.split(' ').map(n => Number(n));
+
+    const result = checkExistence(year, month, day) ? getSeason(month) : -1;
+    console.log(result);
+})
+
+function checkExistence(year, month, day) {
+    const daysOf31 = [1, 3, 5, 7, 8, 10, 12];
+
+    if (daysOf31.includes(month)) {
+        return day <= 31;
+    }
+
+    if (month === 2) {
+        const isLeapYear = year % 4 === 0 && (year !== 100 || year % 400 === 0);
+        return isLeapYear ? day <= 29 : day <= 28;
+    }
+
+    return day <= 30;
+}
+
+function getSeason(month) {
+    const season = {
+        0: 'Winter',
+        1: 'Spring',
+        2: 'Summer',
+        3: 'Fall',
+        4: 'Winter',
+    }
+    return season[Math.floor(month / 3)];
+}
