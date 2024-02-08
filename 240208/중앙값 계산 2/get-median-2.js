a1b01bf2023b7b9f1d00998113ecc6dad9cbc32c
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-02-08 10:58:50 +0900

    Title: 중앙값 계산 2, Time: 202ms, Memory: 47MB, Status: Passed - CodeTree

--- 240208/중앙값 계산 2/get-median-2.js
+++ 240208/중앙값 계산 2/get-median-2.js
@@ -4,13 +4,12 @@ const lines = rawLines.map(line => line.split(' ').map(n => Number(n)));
 const numbers = lines[1];
 console.log(...getMidNumberList(numbers).result);
 
-function getMidNumberList(numbers) {
-
-    return numbers.reduce((board, current, index) => {
+function getMidNumberList(numberList) {
+    return numberList.reduce((board, current, index) => {
         board.array.push(current);
 
         if (index % 2 === 0) {
-            board.array.sort();
+            board.array.sort((a, b) => a - b);
             const mid = board.array[Math.floor(board.array.length / 2)];
             board.result.push(mid);
         }
