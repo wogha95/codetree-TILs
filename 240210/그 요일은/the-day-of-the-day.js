84d7ce6b50392ff9e61ac1e0979a9b27e6647cd8
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-02-10 17:54:58 +0900

    Title: 그 요일은, Time: 302ms, Memory: 30MB, Status: Passed - CodeTree

--- 240210/그 요일은/the-day-of-the-day.js
+++ 240210/그 요일은/the-day-of-the-day.js
@@ -4,8 +4,8 @@ const lines = rawLines.map(line => line.split(' '));
 const [m1, d1, m2, d2] = lines[0].map(n => Number(n));
 const A = lines[1][0];
 
-const [id1, id2] = [getIdNumber(m1, d1), getIdNumber(m2, d2)];
-console.log(Math.ceil((id2 - id1) / 7));
+const [id1, id2] = [getIdNumber(m1, d1) + getGap(A), getIdNumber(m2, d2)];
+console.log(Math.floor((id2 - id1) / 7) + 1);
 
 function getIdNumber(month, day) {
     const monthsOf31 = [1, 3, 5, 7, 8, 10, 12];
@@ -19,4 +19,23 @@ function getIdNumber(month, day) {
         }
         return sum + 30;
     }, 0)
+}
+
+function getGap(str) {
+    switch(str) {
+        case 'Mon':
+            return 0;
+        case 'Tue':
+            return 1;
+        case 'Wed':
+            return 2;
+        case 'Thu':
+            return 3;
+        case 'Fri':
+            return 4;
+        case 'Sat':
+            return 5;
+        case 'Sun':
+            return 6;
+    }
 }
