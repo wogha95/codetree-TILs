713ae0efbf309db6f309fcbfb286c69e9de1977d
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-02-11 14:22:32 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 191ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240211/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.js
+++ 240211/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.js
@@ -5,34 +5,31 @@ const [N] = lines[0].map(n => Number(n));
 const CENTER = Math.floor(N / 2);
 const orders = lines[1][0].split('');
 const square = lines.slice(2).map(line => line.map(n => Number(n)));
-const visited = Array.from({length: N}, () => Array.from({length: N}, () => false));
 
 const result = getResult();
 console.log(result);
 
 function getResult() {
     const [moveR, moveC] = [
-        [-1, 0, -1, 0],
+        [-1, 0, 1, 0],
         [0, 1, 0, -1],
     ];
     let [currentR, currentC] = [CENTER, CENTER];
     let direction = 0;
 
-    visited[CENTER][CENTER] = true;
-
     return orders.reduce((sum, order) => {
-        const newNumber = operate(order);
-        return sum + newNumber;
+        const isMove = operate(order);
+        return sum + (isMove ? square[currentR][currentC] : 0);
     }, square[CENTER][CENTER])
 
     function operate(order) {
         if (order === 'L') {
             direction = (direction + 3) % 4;
-            return 0;
+            return false;
         }
         if (order === 'R') {
             direction = (direction + 1) % 4;
-            return 0;
+            return false;
         }
         if (order === 'F') {
             const isOutRangeR = currentR + moveR[direction] < 0 || N <= currentR + moveR[direction];
@@ -44,10 +41,8 @@ function getResult() {
             if (!isOutRangeC) {
                 currentC = currentC + moveC[direction];
             }
-            const isVisited = visited[currentR][currentC];
-            visited[currentR][currentC] = true;
 
-            return isVisited || isOutRangeR || isOutRangeC ? 0 : square[currentR][currentC];
+            return !isOutRangeR || !isOutRangeC;
         }
     }
 }
