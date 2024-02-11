657d9ad8539c3f44da560027bea67c3fb5d75460
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-02-11 19:09:37 +0900

    Title: 스승의 은혜 3, Time: 188ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240211/스승의 은혜 3/the-grace-form-teacher-3.js
+++ 240211/스승의 은혜 3/the-grace-form-teacher-3.js
@@ -2,29 +2,32 @@ const rawLines = require("fs").readFileSync(0).toString().trim().split("\n")
 const lines = rawLines.map(line => line.split(' ').map(n => Number(n)));
 
 const [N, B] = lines[0];
-const result = getResult(lines.slice(1).sort((a, b) => b.p + b.s - a.p - a.s));
+const result = getResult(lines.slice(1));
 console.log(result);
 
 function getResult(giftList) {
     let maxCount = 0;
 
-    recursive(0, 0, 0, false);
+    for (let index = 0; index < N; index++) {
+        const filteredGiftList = giftList.filter((_, i) => i !== index);
+        recursive(filteredGiftList, giftList[index][0] + giftList[index][1], 1, 0, false);
+    }
 
     return maxCount;
 
-    function recursive(cost, count, currentIndex, isUsedCoupon) {
+    function recursive(filteredGiftList, cost, count, currentIndex, isUsedCoupon) {
         if (B < cost) {
             return;
         }
 
         maxCount = Math.max(maxCount, count);
-        if (currentIndex === N) {
+        if (currentIndex === N - 1) {
             return;
         }
 
-        const [p, s] = giftList[currentIndex];
-        recursive(cost + p + s, count + 1, currentIndex + 1, isUsedCoupon);
-        recursive(cost, count, currentIndex + 1, isUsedCoupon);
-        !isUsedCoupon && recursive(cost + p / 2 + s, count + 1, currentIndex + 1, true);
+        const [p, s] = filteredGiftList[currentIndex];
+        recursive(filteredGiftList, cost + p + s, count + 1, currentIndex + 1, isUsedCoupon);
+        recursive(filteredGiftList, cost, count, currentIndex + 1, isUsedCoupon);
+        !isUsedCoupon && recursive(filteredGiftList, cost + p / 2 + s, count + 1, currentIndex + 1, true);
     }
 }
