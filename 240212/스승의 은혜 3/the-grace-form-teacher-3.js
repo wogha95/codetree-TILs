d938a6f29a63046e6a031d7e904a0b73a11f318d
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-02-12 18:17:58 +0900

    Title: 스승의 은혜 3, Time: 5067ms, Memory: 20MB, Status: Time Limit Exceed - CodeTree

---
+++ 240212/스승의 은혜 3/the-grace-form-teacher-3.js
@@ -0,0 +1,36 @@
+const rawLines = require("fs").readFileSync(0).toString().trim().split("\n")
+const lines = rawLines.map(line => line.split(' ').map(n => Number(n)));
+
+const [N, B] = lines[0];
+const result = getResult(lines.slice(1));
+console.log(result);
+
+function getResult(giftList) {
+    let maxCount = 0;
+    let filteredGiftList = null;
+
+    for (let index = 0; index < N; index++) {
+        filteredGiftList = giftList.filter((_, i) => i !== index).sort((a, b) => a[0] + a[1] - b[0] - b[1]);
+        recursive(giftList[index][0] / 2 + giftList[index][1], 1, 0);
+    }
+
+    return maxCount;
+
+    function recursive(cost, count, currentIndex) {
+        if (B < cost) {
+            return;
+        }
+
+        maxCount = Math.max(maxCount, count);
+        if (currentIndex === N - 1) {
+            return;
+        }
+
+        const [p, s] = filteredGiftList[currentIndex];
+        if (B < cost + p + s) {
+            return;
+        }
+        recursive(cost + p + s, count + 1, currentIndex + 1);
+        recursive(cost, count, currentIndex + 1);
+    }
+}
