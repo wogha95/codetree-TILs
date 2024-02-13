7612d5ff58ad03c41915fc7a206d067f0f1e4662
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-02-13 22:48:29 +0900

    Title: 최대 최소간의 차, Time: 197ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 240213/최대 최소간의 차/difference-between-maximum-and-minimum.js
@@ -0,0 +1,29 @@
+const rawLines = require("fs").readFileSync(0).toString().trim().split("\n")
+const lines = rawLines.map(line => line.split(' ').map(n => Number(n)));
+
+const [N, K] = lines[0];
+const result = getResult(lines[1]);
+console.log(result);
+
+function getResult(numbers) {
+    const [bottom, top] = [Math.min(...numbers), Math.max(...numbers) - 2];
+    let minCost = Number.MAX_SAFE_INTEGER;
+
+    for (let standard = bottom; standard <= top; standard++) {
+        let cost = 0;
+        const [start, end] = [standard, standard + 2];
+        
+        numbers.forEach(value => {
+            if (value < start) {
+                cost += Math.abs(value - start);
+            }
+            if (end < value) {
+                cost += Math.abs(value - end);
+            }
+        })
+
+        minCost = Math.min(minCost, cost);
+    }
+
+    return minCost;
+}
