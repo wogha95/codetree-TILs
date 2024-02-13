0fd1c63164cf6ca812c2ddda1bf2580b42d6b957
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-02-13 21:55:41 +0900

    Title: ABC 줄 세우기, Time: 187ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240213/ABC 줄 세우기/abc-line-up.js
@@ -0,0 +1,29 @@
+const rawLines = require("fs").readFileSync(0).toString().trim().split("\n")
+const lines = rawLines.map(line => line.split(' '));
+
+const [N] = lines[0];
+const result = getResult(lines[1]);
+console.log(result);
+
+function getResult(alphabetList) {
+    const codeList = alphabetList.map(a => a.charCodeAt(0));
+    let count = 0;
+
+    while (true) {
+        let isChanged = false;
+        for (let index = 0; index < N - 1; index++) {
+            if (codeList[index] > codeList[index + 1]) {
+                const temp = codeList[index];
+                codeList[index] = codeList[index + 1];
+                codeList[index + 1] = temp;
+                count += 1;
+                isChanged = true;
+            }
+        }
+        if (isChanged) {
+            break;
+        }
+    }
+
+    return count;
+}
